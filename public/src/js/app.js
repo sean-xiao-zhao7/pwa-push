4cdff2bc84fad75abe373e6e01b318134b32d7fb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-08-17 14:11:10 -0400

    Add local notification

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,43 +1,57 @@
-
 var deferredPrompt;
-var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+var enableNotificationsButtons = document.querySelectorAll(
+    ".enable-notifications"
+);
 
 if (!window.Promise) {
-  window.Promise = Promise;
+    window.Promise = Promise;
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(function () {
-      console.log('Service worker registered!');
-    })
-    .catch(function(err) {
-      console.log(err);
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/sw.js")
+        .then(function () {
+            console.log("Service worker registered!");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+window.addEventListener("beforeinstallprompt", function (event) {
+    console.log("beforeinstallprompt fired");
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
+
+const displayNotification = () => {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.ready().then((workerReg) => {});
+    }
+    let options = {
+        body: "Successfully registered.",
+    };
+    new Notification("Success", options);
+};
+
+function askForNotificationPermission() {
+    Notification.requestPermission(function (result) {
+        console.log("User Choice", result);
+        if (result !== "granted") {
+            console.log("No notification permission granted!");
+        } else {
+            displayNotification();
+        }
     });
 }
 
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
-
-function askForNotificationPermission() {
-  Notification.requestPermission(function(result) {
-    console.log('User Choice', result);
-    if (result !== 'granted') {
-      console.log('No notification permission granted!');
-    } else {
-
+if ("Notification" in window) {
+    for (var i = 0; i < enableNotificationsButtons.length; i++) {
+        enableNotificationsButtons[i].style.display = "inline-block";
+        enableNotificationsButtons[i].addEventListener(
+            "click",
+            askForNotificationPermission
+        );
     }
-  });
-}
-
-if ('Notification' in window) {
-  for (var i = 0; i < enableNotificationsButtons.length; i++) {
-    enableNotificationsButtons[i].style.display = 'inline-block';
-    enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
-  }
 }
